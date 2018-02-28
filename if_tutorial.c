88c5284c4fcfd9e63f128f215f525829d4d12d33
nakadasanda <nakadasanda@outlook.com>
2018-02-28 09:00:32 +0900

    add if

---
+++ if_tutorial.c
@@ -0,0 +1,16 @@
+﻿#include <stdio.h>
+#include <stdbool.h>
+
+int main(){
+   bool onigiri;
+   onigiri = true;
+
+   printf("霊夢は、コンビニに行きました。\n");
+   printf("霊夢は、おにぎりを探しました。\n");
+   if(onigiri){
+      printf("霊夢は、おにぎりを買いました。\n");
+   }else {
+      printf("霊夢は、おにぎりがなかったので、メロンパンを買いました。\n");
+   }
+   return 0;
+}
