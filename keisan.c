6a7edc7779d580ec6459841c5cc7481299361491
nakadasanda <nakadasanda@outlook.com>
2018-02-28 15:37:15 +0900

    fix

--- keisan.c
+++ keisan.c
@@ -6,11 +6,9 @@ int main(void){
 	a = 1;
 	b = 4;
 	sum = a+b;
-	printf("%d + %d = %d",a,b,sum);
-<<<<<<< HEAD
-	printf("%d + %d = %d",a,b,a+b);
-=======
+	printf("%d + %d = %d\n",a,b,sum);
 
->>>>>>> ff26660e685f45ff85eddaed1f9393704931896f
+	printf("%d + %d = %d\n",a,b,a+b);
+
 	return 0;
 }
