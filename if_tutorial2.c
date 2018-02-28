5ce3869c39dc90c4d04607f26b79b2277ecb40f1
nakadasanda <nakadasanda@outlook.com>
2018-02-28 15:20:08 +0900

    add if_tutorial.c

---
+++ if_tutorial2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+   int age =16;
+   if(age >= 20 ){
+      printf("‚¨Žð‚ðˆù‚ñ‚Å‚à‚¢‚¢‚æ\n");
+   }else if(age < 20){
+      printf("‚¨Žð‚Í‚Ü‚¾‘‚¢‚æ\n");
+   }
+   return 0;
+}
